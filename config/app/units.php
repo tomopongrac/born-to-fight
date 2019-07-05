ca49c1d4bc420dbb4bfe7f85e3999321cdabaaca
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2019-07-05 23:13:56 +0200

    Creating config file with units stats

---
+++ config/app/units.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    [
+        'type' => 'Warrior',
+        'strength' => 10,
+        'armour' => 3,
+        'accuracy' => 20,
+        'maxHealth' => 50,
+    ],
+    [
+        'type' => 'Archer',
+        'strength' => 7,
+        'armour' => 1,
+        'accuracy' => 50,
+        'maxHealth' => 30,
+    ],
+    [
+        'type' => 'Knight',
+        'strength' => 20,
+        'armour' => 5,
+        'accuracy' => 10,
+        'maxHealth' => 100,
+    ],
+];
